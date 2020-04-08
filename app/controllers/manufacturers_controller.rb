2561fbb9d82c827f08af104190e0980c3cb90d5f
Renato Santini <renato.santini@gmail.com>
2020-04-08 16:16:09 -0300

    Visitor view manufacturers

---
+++ app/controllers/manufacturers_controller.rb
@@ -0,0 +1,9 @@
+class ManufacturersController < ApplicationController
+    def index
+        @manufacturers = Manufacturer.all
+    end
+    def show
+        id = params[:id]
+        @manufacturer = Manufacturer.find(id)
+    end
+end
