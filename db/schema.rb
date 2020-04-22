985c34a458faeecc1eb169e75adb5f0cb7d79d7d
Renato Santini <renato.santini@gmail.com>
2020-04-21 23:11:15 -0300

    User register customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_222414) do
+ActiveRecord::Schema.define(version: 2020_04_22_012058) do
 
   create_table "car_categories", force: :cascade do |t|
     t.string "name"
     t.decimal "daily_rate"
     t.decimal "car_insurance"
     t.decimal "third_party_insurance"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "document"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ spec/features/user_register_customer_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User register customer' do 
+  scenario 'succesfully' do
+    visit root_path
+    click_on 'Cadastre-se'
+
+    fill_in "Nome",	with: "João Ubaldo Ribeiro" 
+    fill_in "CPF",	with: "00000000000" 
+    fill_in "email",	with: "joaoubaldo@gmail.com" 
+    click_on 'Cadastrar'
+
+    expect(current_path).to eq customer_path(Customer.last.id)
+    expect(page).to have_content("João Ubaldo Ribeiro")
+    expect(page).to have_content("00000000000")
+    expect(page).to have_content("joaoubaldo@gmail.com")
+    expect(page).to have_link('Voltar', href: root_path)
+  end
+end
